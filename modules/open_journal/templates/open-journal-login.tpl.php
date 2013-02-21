252022c0b2bba7e99b5378d0d9c8f77772bc6a10
rit <ruangritboonrod@gmail.com>
2013-02-21 19:43:20 +0700

    login page

---
+++ modules/open_journal/templates/open-journal-login.tpl.php
@@ -0,0 +1,5 @@
+<div class = "open-journal-login">
+	<?php print $form;?>
+	<h3>Member condition</h3>
+	<div><?php print $condition;?></div>
+</div>
---
+++ themes/clean_journal/page.tpl.php
@@ -0,0 +1,31 @@
+
+  <?php 
+  if(!empty($open_journal_logo2)):?>
+    <div id="open-journal-sign-logo">
+      <img src="<?php print($open_journal_logo2);?>" alt="<?php print $site_name; ?>" />
+    </div>
+  <?php endif;?>
+
+  <?php 
+  if(!empty($open_journal_logo1)):?>
+    <div id="open-journal-small-logo">
+      <?php print($open_journal_logo1);?>
+    </div>
+  <?php endif;?>
+
+
+  <?php if ($title && !$disabled_title): ?>
+    <h1 class="page-title"><?php print $title; ?></h1>
+  <?php endif; ?>
+
+
+  <div id="page">
+    <div id="content" class="clearfix">
+      <?php if ($messages): ?>
+        <div id="console" class="clearfix"><?php print $messages; ?></div>
+      <?php endif; ?>
+      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+      <?php print render($page['content']); ?>
+    </div>
+
+  </div>
