26a80b32f4dcb30915081de8a06050655f119ec5
Aegir <aegir@local>
2013-03-05 21:15:27 +0700

    fixed rit bug

--- modules/open_journal/templates/open-journal-overview-sidebar.tpl.php
+++ modules/open_journal/templates/open-journal-overview-sidebar.tpl.php
@@ -32,7 +32,7 @@
         </li>
         <?php else:?>
         <li class="clickable-people owner profile-picture-small">
-          <?php if($GLOBALS['user']->uid == $list['uid']):?>
+          <?php if($information->uid == $list['uid']):?>
             <?php print l($list['user_info']['display_picture'].'<span>Owner</span>', '' ,array('html' => TRUE))?>
           <?php else:?>
             <?php print l($list['user_info']['display_picture'], '', array('html' => TRUE))?>  
@@ -60,40 +60,54 @@
     </div>
     <?php endif;?>
 
+    <?php if ($information->subject_en): ?>
   	<div class="information-field subject-en-field">
   		<span class="label">Keyword:</span>
   		<span class="value"><?php print $information->subject_en;?></span>
   	</div>
+    <?php endif;?>
 
+    <?php if ($information->dc_creator): ?>
   	<div class="information-field dc-creator-field">
   		<span class="label">Creators: </span>
   		<span class="value"><?php echo $information->dc_creator;?></span>
   	</div>
+    <?php endif;?>
   	
+    <?php if ($information->dc_publisher): ?>
   	<div class="information-field dc-publisher-field">
   		<span class="label">Publishers: </span>
   		<span class="value"><?php print $information->dc_publisher?></span>
   	</div>
+    <?php endif;?>
   	
+    <?php if ($information->dc_contributor): ?>
   	<div class="information-field dc-contributor-field">
   		<span class="label">Contributors: </span>
   		<span class="value"><?php print $information->dc_contributor;?></span>
   	</div>
+    <?php endif;?>
 
+    <?php if ($information->dc_date): ?>
   	<div class="information-field date-field">
   		<span class="label">Date: </span>
   		<span class="value"><?php print date('d M Y' ,$information->dc_date);?></span>
   	</div>
+    <?php endif;?>
 
+    <?php if ($information->dc_language): ?>
   	<div class="information-field language-field">
   		<span class="label">Language: </span>
   		<span class="value"><?php print $information->dc_language?></span>
   	</div>
+    <?php endif;?>
 
+    <?php if ($information->dc_identifier): ?>
   	<div class="information-field dc-identifier-field">
   		<span class="label">Final URL: </span>
   		<span class="value"><?php print $information->dc_identifier;?></span>
   	</div>
+    <?php endif;?>
   	
   	
 
