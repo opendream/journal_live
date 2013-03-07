8fcbd5579c3ec0785beff3a3f0afd6dec85a8d6e
rit <ruangritboonrod@gmail.com>
2013-03-07 22:37:40 +0700

    journal list not done and meet the dragon

--- modules/open_journal/templates/open-journal-list.tpl.php
+++ modules/open_journal/templates/open-journal-list.tpl.php
@@ -1,8 +1,8 @@
 <div id="user-heading">
 	<h1>Journals List</h1>
-	<?php print $journal_list['user_info']['display_picture'];?>
-	<h2><?php print $journal_list['user_info']['display_name'];?></h2>
-	<span>4 Journals</span>
+	<?php print $user_info['display_picture'];?>
+	<h2><?php print $user_info['display_name'];?></h2>
+	<span><?php print $count_journal; ?> Journals</span>
 </div>
 
 <div class="journal-filter-tab">
@@ -23,36 +23,17 @@
 
 <div id="journal-list">
 	<ul>
-		<?php foreach ($journal_list['list'] as $value):?>
-		<li class="journal-item">
-			<div class="journal-status status<?php echo $value->status;?>"><span><?php print $value->status_name;?></span></div>
-			<h3 class="journal-title"><a href="/journal/1/overview"><?php print $value->dc_title;?></a></h3>
-			<div class="journal-update">
-				<span class="label">Status update: </span>
-				<span><?php print date('d M Y', $value->status_changed);?></span>
-			</div>
-			<div class="journal-create">
-				<span class="label">Create Date: </span>
-				<span><?php print date('d M Y', $value->created);?></span>		
-			</div>
-			<ul class="relate-peoples">
-				<?php foreach ($value->list_people as $key_info => $value_info):?>
-				<?php if($key_info == 'more'):?>
-				<li class="more-people"><?php print l($value_info['display_picture'], '');?></li>
-				<?php elseif($value_info->uid == $value->uid):?>
-				<li class="clickable-people owner profile-picture-small">
-
-					<?php print l($value_info->user_info['display_picture'].'<span>Owner</span>', '' ,array('html' => TRUE))?>
-				<?php else:?>
-				<li class="clickable-people  profile-picture-small">
-					<?php print l($value_info->user_info['display_picture'], '' ,array('html' => TRUE))?>
-				<?php endif;?>
-				</li>
-				<?php endforeach;?>
-				
-			</ul>
-		</li>
-		<?php endforeach;?>	
+		<?php foreach ($journal_list as $journal): ?>
+			<li class="journal-item"><?php print theme('open_journal_list_item', array('journal' => $journal)); ?><li>
+		<?php endforeach ?>
 	</ul>
-	<div class="load-more"><a href="#">Load More …</a></div>
+	<div class="load-more">
+		<?php print l('Load More ...', url(OPEN_JOURNAL_PREFIX_PATH.'/ajax/journal/list/'.$journal->jid.'/'.$limit) ,array(
+			'attributes' => array(
+				'id' => 'journal-list-load-more',
+		 	))
+		);
+		?>
+	</div>
+
 </div>
