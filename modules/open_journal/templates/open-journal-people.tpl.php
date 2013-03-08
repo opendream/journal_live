dc89fe8e163d8283668a9708b838aa718f902eab
Aegir <aegir@local>
2013-03-08 14:43:39 +0700

    add people template

---
+++ modules/open_journal/templates/open-journal-people.tpl.php
@@ -0,0 +1,108 @@
+<!-- People -->
+<div class="journal-block people-block block-bg-shadow">
+
+  <h2 class="journal-block-title">People</h2>
+  
+  <!-- Creator Section -->
+  <div class="journal-block-content people-creator">
+  
+  	<h3 class="people-section-title">Creator</h3>
+  	
+  	<!-- People item -->
+  	<div class="journal-user">
+    	<a href="/"><img class="profile-picture-big" src="/sites/all/themes/clean_journal/images/dummy/big-picture-profile-tarongpong.png" alt="researcher1" title="researcher1"></a>
+    	<h3 class="username"><a href="/">Benz Gimmik</a></h3>
+    	<div class="join-date">
+  			<span class="label">Join Date: </span>
+  			<span>05 Mar 2013</span>		
+  		</div>
+    </div><!-- End People item -->
+    
+  </div><!-- End Creator Section -->
+
+  <!-- Creator Section -->
+  <div class="journal-block-content people-contributor">
+  
+  	<h3 class="people-section-title">Contributor</h3>
+  	
+  	<!-- People item -->
+  	<ul>
+  	
+  		<li class="journal-user active-people">
+  			<a href="/"><img class="profile-picture-big" src="/sites/all/themes/clean_journal/images/dummy/big-picture-profile-panu.png" alt="researcher1" title="researcher1"></a>
+  			<h3 class="username"><a href="/">Panu Meesuk</a></h3>
+    		<div class="join-date">
+  				<span class="label">Join Date: </span>
+  				<span>05 Mar 2013</span>		
+  			</div>
+  			<a class="active-people-btn" href="/">active</a>
+  		</li>
+  		
+  		<li class="journal-user active-people">
+  			<a href="/"><img class="profile-picture-big" src="/sites/all/themes/clean_journal/images/dummy/big-picture-profile-panu.png" alt="researcher1" title="researcher1"></a><h3 class="username"><a href="/">Panu Meesukdeemak</a></h3>
+    		<div class="join-date">
+  				<span class="label">Join Date: </span>
+  				<span>05 Mar 2013</span>		
+  			</div>
+  			<a class="active-people-btn" href="/">active</a>
+  		</li>
+  		
+  		<li class="journal-user active-people">
+  			<a href="/"><img class="profile-picture-big" src="/sites/all/themes/clean_journal/images/dummy/big-picture-profile-panu.png" alt="researcher1" title="researcher1"></a><h3 class="username"><a href="/">Panu Meesuk</a></h3>
+    		<div class="join-date">
+  				<span class="label">Join Date: </span>
+  				<span>05 Mar 2013</span>		
+  			</div>
+  			<a class="active-people-btn" href="/">active</a>
+  		</li>  
+  				
+  		<li class="journal-user active-people">
+  			<a href="/"><img class="profile-picture-big" src="/sites/all/themes/clean_journal/images/dummy/big-picture-profile-pirapa.png" alt="researcher1" title="researcher1"></a><h3 class="username"><a href="/">Pirapa Sonmeesoap</a></h3>
+    		<div class="join-date">
+  				<span class="label">Join Date: </span>
+  				<span>05 Mar 2013</span>		
+  			</div>
+  			<a class="active-people-btn" href="/">active</a>
+  		</li>
+  		
+  		<li class="journal-user active-people">
+  			<a href="/"><img class="profile-picture-big" src="/sites/all/themes/clean_journal/images/dummy/big-picture-profile-pirapa-gray.png" alt="researcher1" title="researcher1"></a><h3 class="username"><a href="/">Pirapa Sonmeesoap</a></h3>
+    		<div class="join-date">
+  				<span class="label">Join Date: </span>
+  				<span>05 Mar 2013</span>		
+  			</div>
+  			<a class="inactive-people-btn" href="/">inactive</a>
+  		</li>
+  		
+  		<li class="journal-user">
+  			<a href="/"><img class="profile-picture-big" src="/sites/all/themes/clean_journal/images/dummy/big-picture-profile-pirapa-gray.png" alt="researcher1" title="researcher1"></a><h3 class="username"><a href="/">Pirapa Sonmeesoap</a></h3>
+    		<div class="join-date">
+  				<span class="label">Join Date: </span>
+  				<span>05 Mar 2013</span>		
+  			</div>
+  			<a class="inactive-people-btn" href="/">inactive</a>
+  		</li>
+  		
+  		<li class="journal-user">
+  			<a href="/"><img class="profile-picture-big" src="/sites/all/themes/clean_journal/images/dummy/big-picture-profile-nutchaya-gray.png" alt="researcher1" title="researcher1"></a><h3 class="username"><a href="/">Nutchaya Nawapornpaipan</a></h3>
+    		<div class="join-date">
+  				<span class="label">Join Date: </span>
+  				<span>05 Mar 2013</span>		
+  			</div>
+  			<a class="inactive-people-btn" href="/">inactive</a>
+  		</li>
+  		
+  		<li class="journal-user">
+  			<a href="/"><img class="profile-picture-big" src="/sites/all/themes/clean_journal/images/dummy/big-picture-profile-nutchaya-gray.png" alt="researcher1" title="researcher1"></a><h3 class="username"><a href="/">Nutchaya Nawapornpaipan</a></h3>
+    		<div class="join-date">
+  				<span class="label">Join Date: </span>
+  				<span>05 Mar 2013</span>		
+  			</div>
+  			<a class="inactive-people-btn" href="/">inactive</a>
+  		</li>
+  		
+  	</ul><!-- End People item -->
+    
+  </div><!-- End Creator Section -->
+  
+</div><!-- End People -->
