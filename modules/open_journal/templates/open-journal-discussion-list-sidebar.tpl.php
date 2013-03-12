60e8feb9634c429f5ea045968965a025bf619687
rit <ruangritboonrod@gmail.com>
2013-03-12 21:10:15 +0700

    discussion sidebar not done

--- modules/open_journal/templates/open-journal-discussion-list-sidebar.tpl.php
+++ modules/open_journal/templates/open-journal-discussion-list-sidebar.tpl.php
@@ -1,27 +1,25 @@
 <div>
 	<h2>Status</h2>
 	<?php foreach($status_list as $key_status => $value_status):?>
-	<?php if($key_status != 4):?>	
-		<div 
-		<?php if($key_status < $journal->status):print " class='status-active' ";
-		elseif($key_status == $journal->status):print " class='status-active current' ";
-		else:print " class='status-disabled' ";endif;?>
-		>
-		
-		<div class="discussion-sidebar-status-<?php print $key_status;?>" >
-		<?php print $key_status+1;?>
-		</div>
+	<div 
+	<?php if($key_status < $journal->status):print " class='status-active' ";
+	elseif($key_status == $journal->status):print " class='status-active current' ";
+	else:print " class='status-disabled' ";endif;?>
+	>
+	
+	<div class="discussion-sidebar-status-<?php print $key_status;?>" >
+	<?php print $key_status+1;?>
+	</div>
 
-		<?php print $value_status;?>
-		<?php if($journal->status == $key_status):?>
-			<a href="">Approve</a>
-		<?php endif;?>	
-		</div>
+	<?php print $value_status;?>
+	<?php if($journal->status == $key_status && $journal->status != count($status_list)-1):?>
+		<?php print $approve_form;?>
 	<?php endif;?>	
+	</div>
 
 	<?php endforeach;?>
+	<?php print $reject_form;?>
+	<?php print 'or '.l('Reject' ,'', array('attributes' => array('id' => 'reject_bottom')));?>
 
-	<div>
-	or <a href="#">Reject</a>
-	</div>
+	
 </div>
