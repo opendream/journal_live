b3b4763cd78476d1454255e47cdebc12e3e90bff
rit <ruangritboonrod@gmail.com>
2013-03-12 16:22:50 +0700

    discussion not done

--- modules/open_journal/templates/open-journal-discussion-list-sidebar.tpl.php
+++ modules/open_journal/templates/open-journal-discussion-list-sidebar.tpl.php
@@ -1,22 +1,23 @@
 <div>
 	<h2>Status</h2>
 	<?php foreach($status_list as $key_status => $value_status):?>
-	
-	<div 
-	<?php if($key_status < $journal->status):print " class='status-active' ";
-	elseif($key_status == $journal->status):print " class='status-active current' ";
-	else:print " class='status-disabled' ";endif;?>
-	>
-	
-	<div class="discussion-sidebar-status-<?php print $key_status;?>" >
-	<?php print $key_status+1;?>
-	</div>
+	<?php if($key_status != 4):?>	
+		<div 
+		<?php if($key_status < $journal->status):print " class='status-active' ";
+		elseif($key_status == $journal->status):print " class='status-active current' ";
+		else:print " class='status-disabled' ";endif;?>
+		>
+		
+		<div class="discussion-sidebar-status-<?php print $key_status;?>" >
+		<?php print $key_status+1;?>
+		</div>
 
-	<?php print $value_status;?>
-	<?php if($journal->status == $key_status):?>
-		<a href="">Approve</a>
+		<?php print $value_status;?>
+		<?php if($journal->status == $key_status):?>
+			<a href="">Approve</a>
+		<?php endif;?>	
+		</div>
 	<?php endif;?>	
-	</div>
 
 	<?php endforeach;?>
 
--- modules/open_journal/templates/open-journal-discussion-list.tpl.php
+++ modules/open_journal/templates/open-journal-discussion-list.tpl.php
@@ -5,7 +5,7 @@
 <?php foreach($discussion_list as $discussion_item):?>
     <div class="journal-detail-content-item">
       <div><?php print $discussion_item->user['display_picture'];?> <span><?php print $discussion_item->user['display_name'];?></span> </div>
-      <div><?php print $discussion_item->title;?></div>
+      <div><?php print l($discussion_item->title, OPEN_JOURNAL_PREFIX_PATH.'/'.$jid.'/discussion/'.$discussion_item->did);?></div>
       <div><?php print $discussion_item->description;?></div>
     </div>
 <?php endforeach;?>
