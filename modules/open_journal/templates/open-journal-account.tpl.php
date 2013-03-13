8542b0337fc05842fefc56b2bb250ea1c3792290
Aegir <aegir@local>
2013-03-14 00:48:00 +0700

    add css

--- modules/open_journal/templates/open-journal-account.tpl.php
+++ modules/open_journal/templates/open-journal-account.tpl.php
@@ -1,7 +1,17 @@
 <div class="account-profile block-bg-shadow">
-	<div><?php print $account->user_info['display_picture'];?></div>
-	<div>Username: </div><div><?php print $account->name;?></div>
-	<div>First name: </div><div><?php print $account->data['firstname'];?></div>
-	<div>Last name: </div><div><?php print $account->data['lastname'];?></div>
-	<div>E-mail address: </div><div><?php print $account->mail;?></div>
+	<?php print $account->user_info['display_picture'];?>
+	<div class="account-profile-content">
+		<div class="account-profile-item">
+			<div class="account-profile-label label">Username: </div><div><?php print $account->name;?></div>
+		</div>
+		<div class="account-profile-item">
+			<div class="account-profile-label label">First name: </div><div><?php print $account->data['firstname'];?></div>
+		</div>
+		<div class="account-profile-item">
+			<div class="account-profile-label label">Last name: </div><div><?php print $account->data['lastname'];?></div>
+		</div>
+		<div class="account-profile-item">
+			<div class="account-profile-label label">E-mail address: </div><div><?php print $account->mail;?></div>
+		</div>
+	</div>
 </div>
