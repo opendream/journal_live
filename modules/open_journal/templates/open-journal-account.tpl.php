375b1823049046655abb2510e00394f408d38266
Aegir <aegir@local>
2013-03-13 21:38:49 +0700

    add css

--- modules/open_journal/templates/open-journal-account.tpl.php
+++ modules/open_journal/templates/open-journal-account.tpl.php
@@ -1,10 +1,7 @@
-<div>
+<div class="account-profile block-bg-shadow">
 	<div><?php print $account->user_info['display_picture'];?></div>
 	<div>Username: </div><div><?php print $account->name;?></div>
 	<div>First name: </div><div><?php print $account->data['firstname'];?></div>
 	<div>Last name: </div><div><?php print $account->data['lastname'];?></div>
 	<div>E-mail address: </div><div><?php print $account->mail;?></div>
-	
-	
-
 </div>
