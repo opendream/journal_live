53ba434132bd77ad5e87ac4bf1f51f458b961291
rit <ruangritboonrod@gmail.com>
2013-03-13 23:39:08 +0700

    display file picture when discussion have file

--- modules/open_journal/templates/open-journal-discussion-list.tpl.php
+++ modules/open_journal/templates/open-journal-discussion-list.tpl.php
@@ -10,7 +10,15 @@
         <?php endif ?>
 	      
 	      <div class="journal-user"><?php print $discussion_item->user['display_picture'];?> <span><?php print $discussion_item->user['display_name'];?></span> </div>
-	      <div class="discus-title"><?php print l($discussion_item->title, OPEN_JOURNAL_PREFIX_PATH.'/'.$jid.'/discussion/'.$discussion_item->did);?></div>
+	      <div class="discus-title">
+	      	
+	      	<?php if($discussion_item->fid):?>
+	      		<?php  print l($discussion_item->title.'<span class="file-attach"><img src="/sites/all/modules/open_journal/images/ic-has-file.png" alt="has file attach" /></span>', OPEN_JOURNAL_PREFIX_PATH.'/'.$jid.'/discussion/'.$discussion_item->did, array('html' => TRUE));?>
+	      	<?php else:?>
+	      		<?php  print l($discussion_item->title, OPEN_JOURNAL_PREFIX_PATH.'/'.$jid.'/discussion/'.$discussion_item->did, array('html' => TRUE));?>
+	      	<?php endif;?>
+	      		
+	      </div>
 	      <div class="discus-content"><?php print $discussion_item->description;?></div>
 	    </div>
 	<?php endforeach;?>
