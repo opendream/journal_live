aafc57289027068f0a1b25345c13da77ae94fea3
rit <ruangritboonrod@gmail.com>
2013-03-18 13:30:59 +0700

    edit display wording from comment

--- modules/open_journal/templates/open-journal-register.tpl.php
+++ modules/open_journal/templates/open-journal-register.tpl.php
@@ -1,7 +1,7 @@
 <div class = "open-journal-register">
   <?php print $form;?>
   <div id="member-condition">
-  	<h3>Member condition</h3>
+  	<h3>Member Conditions</h3>
   	<div><?php print $condition;?></div>
   </div>
 </div>
