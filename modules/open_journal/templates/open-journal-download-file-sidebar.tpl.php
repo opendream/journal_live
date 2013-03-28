6d32a5ebce648c053d1eb4ac0c09310756e8d2a6
rit <ruangritboonrod@gmail.com>
2013-03-28 10:58:06 +0700

    fix comment not done

---
+++ modules/open_journal/templates/open-journal-download-file-sidebar.tpl.php
@@ -0,0 +1,9 @@
+<div class="download-file-sidebar-block block-bg-shadow"> 
+	
+	<h2 class="journal-block-title">Download reviewer form</h2>
+
+	<div>
+		<?php print l('Dowload', drupal_get_path('module', 'open_journal').'/files/reviewer_form.zip', array('attributes' => array('class' => 'file-download-sidebar-btn')));?>
+	</div>
+
+</div>
